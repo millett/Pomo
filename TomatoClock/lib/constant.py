0c8fec8b4ed1f42fbab61bf6dd224659aebef249
Ali Gottschall <aligottschall@protonmail.com>
2019-08-13 23:42:36 +0200

    added addon id

--- TomatoClock/lib/constant.py
+++ TomatoClock/lib/constant.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 from anki.lang import currentLang
 
-ADDON_CD = "addon int num"
+ADDON_CD = 811976365
 HAS_SET_UP = False
-DEBUG = True
+DEBUG = False
 MIN_SECS = 1 if DEBUG else 60
 __version__ = "0.1"
-#STATISTICS_PY = "https://raw.githubusercontent.com/upday7/TomatoClock/master/TomatoClock/lib/tomatostats.py"
-REST_MINS = 5 # get out of config/settings
+REST_MINS = 5
 
 UPDATE_LOGS = ()
