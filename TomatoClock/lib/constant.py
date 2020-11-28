a8797089a160c70173ca5e74e6e99333a8f20443
Matthew Millett <millett@stanford.edu>
2020-11-28 12:16:33 -0600

    update vote button

--- TomatoClock/lib/constant.py
+++ TomatoClock/lib/constant.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from anki.lang import currentLang
 
-ADDON_CD = 811976365
+ADDON_CD = 2018952444
 HAS_SET_UP = False
 DEBUG = False
 MIN_SECS = 1 if DEBUG else 60
 __version__ = "0.1"
-REST_MINS = 5
 
 UPDATE_LOGS = ()
